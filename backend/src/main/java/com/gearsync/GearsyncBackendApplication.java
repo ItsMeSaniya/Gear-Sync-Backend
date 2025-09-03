c37dd0dbeaa98201855078a1b3780360118c4309
Faskath513 <faskath9054@gmail.com>
2025-09-03 10:58:06 +0530

    Set up file structure

--- backend/src/main/java/com/gearsync/GearsyncBackendApplication.java
+++ backend/src/main/java/com/gearsync/GearsyncBackendApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class PosBackendApplication {
+public class GearsyncBackendApplication {
     public static void main(String[] args) {
-        SpringApplication.run(PosBackendApplication.class, args);
+        SpringApplication.run(GearsyncBackendApplication.class, args);
     }
 }
